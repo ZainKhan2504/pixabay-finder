9170be0df95228971ea1ba098be1f55d3c21de51
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2019-01-01 17:06:26 +0500

    Deploy Commit

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 class Search extends Component {
   state = {
@@ -14,21 +15,25 @@ class Search extends Component {
   };
 
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then(res => this.setState({ images: res.data.hits }))
-        .catch(err => console.log(err));
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === "") {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
     });
   };
   onAmountChange = (e, index, value) => this.setState({ amount: value });
 
   render() {
-    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -52,6 +57,9 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
